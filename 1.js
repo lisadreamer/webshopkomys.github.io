201e5918260c65720bb5ce238a8f6379d958710b
Petrova Elizaveta <a1504523@myy.haaga-helia.fi>
2016-11-09 21:45:04 +0200

    first step corrected

--- 1.js
+++ 1.js
@@ -1,4 +1,40 @@
-jQuery(document).ready(function($){
+
+function checkPasswords() {
+    
+    var password1 = document.getElementById("pw1");
+    var password2 = document.getElementById("pw2");
+    if (password1.value !== password2.value) {
+        console.log("111");
+        password2.setCustomValidity('Passwords do not match'); } 
+    else {console.log("22");
+        password2.setCustomValidity(''); }
+ 	}
+
+checkPasswords();
+
+var colorInputField = document.querySelector("#colorChooser");
+    
+    colorInputField.addEventListener('input', function(evt) {      
+        document.body.style.backgroundColor = this.value;
+      }, false);
+
+//window.onload = function(){ 
+        
+        var field = document.getElementById("db");
+     
+        field.oninput = function(evt) {
+            var date = this.value;
+            if(this.valueAsDate <= new Date()) {
+                 console.log("birthdate is past");
+                field.style.backgroundColor= "white";
+            } else {
+                console.log("birthdate in future");
+                field.style.backgroundColor= "lightpink";
+            }
+        }
+   // };
+        
+/*jQuery(document).ready(function($){
  // Defining a function to set size for #hero 
     function fullscreen(){
         jQuery('#hero').css({
@@ -14,4 +50,5 @@ jQuery(document).ready(function($){
        fullscreen();         
     });
 
-});
+});*/
+
